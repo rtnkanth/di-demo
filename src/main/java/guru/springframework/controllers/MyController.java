9de1b31cc96fa6f26851915ba93c39eae798221e
rtnkanth <rtnkanth@gmail.com>
2019-11-18 12:46:53 +0530

    Application Context Addition

---
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -0,0 +1,11 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String hello(){
+        System.out.println("Hello!!!");
+         return "foo";
+    }
+}
