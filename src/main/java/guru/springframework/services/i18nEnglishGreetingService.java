20f5361360ffe50efb3454d2d2dbb396b25739d8
rtnkanth <rtnkanth@gmail.com>
2020-11-03 15:11:06 +0530

    Property Injected Controller Test Addition

--- src/main/java/guru/springframework/services/i18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/services/i18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN" , "default"})
 @Service("i18nService")
 public class i18nEnglishGreetingService implements GreetingService {
 
